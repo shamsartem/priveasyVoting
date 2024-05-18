ad22aeab8e7347c2eb73c8488c05376a0ad6683f
Artsiom Shamsutdzinau <shamsartem@gmail.com>
2024-05-18 10:43:06 +0200

    set up page structure

---
+++ frontend/src/pages/Results.tsx
@@ -0,0 +1,10 @@
+import { Nav } from "../components/Nav";
+
+export function Results() {
+  return (
+    <div>
+      <Nav />
+      <h2>Results</h2>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Vote.tsx
@@ -0,0 +1,10 @@
+import { Nav } from "../components/Nav";
+
+export function Vote() {
+  return (
+    <div>
+      <Nav />
+      <h2>Vote</h2>
+    </div>
+  );
+}
